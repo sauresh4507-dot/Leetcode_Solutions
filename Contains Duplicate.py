084953428b47e77a66908369b97030145a8f5f37
sauresh4507-newbie <saureshsdbsc24@rvu.edu.in>
2026-02-06 10:49:16 +0530

    first

---
+++ Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        for i in range(i,len(nums)):
+            for j in range(i+1,len(nums)):
+                if num[i] == num[j]:
+                    return true
+                return false
+        
